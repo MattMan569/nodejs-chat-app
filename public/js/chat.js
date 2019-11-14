aae48b51b00797737b99b4d4458a35f82c540670
MattMan569 <matthewpolsom@hotmail.com>
2019-11-14 15:01:39 -0600

    add geolocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,21 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const input = e.target.elements.message;
     const message = input.value;
-    socket.emit('sendMessage', message);
+    socket.emit('sendMessage', message, () => {
+        console.log('The message was delivered!');
+    });
     input.value = '';
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        });
+    });
 });
