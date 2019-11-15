36b3050a2faad988cc75e9bdf39b5482366ed27d
MattMan569 <matthewpolsom@hotmail.com>
2019-11-14 18:33:47 -0600

    send location messages with a link

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,12 +10,22 @@ const $messages = document.querySelector('#messages');
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
 
 socket.on('message', (message) => {
     console.log(message);
     // eslint-disable-next-line no-undef
     const html = Mustache.render(messageTemplate, {
         message,
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('locationMessage', (url) => {
+    console.log(url);
+    // eslint-disable-next-line no-undef
+    const html = Mustache.render(locationMessageTemplate, {
+        url,
     });
     $messages.insertAdjacentHTML('beforeend', html);
 });
